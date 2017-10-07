98a56b4471385973d426920c81b677e70ce5031f
Xillow <Matthewbee1@gmail.com>
2017-10-07 15:17:37 -0400

    Update Door.py

--- Door.py
+++ Door.py
@@ -1,10 +1,21 @@
-CHOICE_1 = "door"
-CHOICE_2 = "enter"
+CHOICE_1 = "enter"
+CHOICE_2 = "close"
 
 def door():
+    
     decision = ""
+    count 
+    
+    #while loop checks to make sure you have selected a correct option
     while (decision != CHOICE_1) or(decision != CHOICE_2):
         
+        if decision == CHOICE_1:
         
+        
+        elif decision == CHOICE_2:
+        
+    	    
+        else:
+            print( "No, No, No stanley please select the correct option")
 		
 		
