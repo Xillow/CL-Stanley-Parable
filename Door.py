bec1ffd0b90a04d7c2091b12130c315c749c9aba
Xillow <Matthewbee1@gmail.com>
2017-10-07 15:30:10 -0400

    Update Door.py

--- Door.py
+++ Door.py
@@ -1,21 +1,25 @@
 CHOICE_1 = "enter"
 CHOICE_2 = "close"
+MISTAKE_LIM = 10
 
 def door():
     
     decision = ""
-    count 
+    count = 0
     
     #while loop checks to make sure you have selected a correct option
-    while (decision != CHOICE_1) or(decision != CHOICE_2):
+    while (decision != CHOICE_1) and (decision != CHOICE_2):
         
+	decision = input("what is your choice?(\"enter\" or \"close\")
+	
         if decision == CHOICE_1:
         
         
         elif decision == CHOICE_2:
-        
-    	    
+		
+	elif count > MISTAKE_LIM:
+			 
         else:
             print( "No, No, No stanley please select the correct option")
 		
-		
+	count += 1
