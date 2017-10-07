666bfc4f53cde05f190993d344ad24ae2dc48f74
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-07 18:03:56 -0400

    Update door.py

--- door.py
+++ door.py
@@ -1,6 +1,8 @@
+import window
+import reluctant
 CHOICE_1 = "enter"
 CHOICE_2 = "close"
-MISTAKE_LIM = 5
+MISTAKE_LIM = 4
 
 def door():
     
@@ -9,17 +11,16 @@ def door():
     
     #while loop checks to make sure you have selected a correct option
     while (decision != CHOICE_1) and (decision != CHOICE_2):
-        
-	decision = input("what is your choice?(\"enter\" or \"close\")
+        print("All his co-workers were gone. What could it mean? Stanley decided to go to the meeting room; perhaps he had simply missed a memo.")
+    	decision = input("(what is your choice?(\"enter\" or \"close\")) ")
 	
         if decision == CHOICE_1:
-        
+            window.window()
         
         elif decision == CHOICE_2:
+		    reluctant.reluctant()
+        
+    	elif count > MISTAKE_LIM:
+		    print("Stanley just kept touching doors and desks and pushing buttons meaninglessly, failing to realize that he was not progressing the story at all.")
 		
-	elif count > MISTAKE_LIM:
-			 
-        else:
-            print( "No, No, No stanley please select the correct option")
-		
-	count += 1
+    	count += 1
