af86c29101169e05763df8184dd7b7571830a96f
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-07 18:19:52 -0400

    Create doorchoice.py

---
+++ doorchoice.py
@@ -0,0 +1,16 @@
+import left
+import right
+LEFT = "left"
+RIGHT = "right"
+
+door = "none"
+
+while door != LEFT and door != RIGHT:
+    print("When Stanley came to a set of 2 open doors, he entered the door on his left.")
+    door = input("(What is your choice? (\"yes\" or \"no\"))")
+    
+    if door == LEFT:
+        left.left()
+    if door == RIGHT:
+        right.right()
+        
