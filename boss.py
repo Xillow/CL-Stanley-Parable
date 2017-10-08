5c1b201293745b1ee54fdc2777fe061210a5f595
twinkert <thomas.winkert@gmail.com>
2017-10-08 02:32:40 -0400

    Update boss.py

--- boss.py
+++ boss.py
@@ -1,2 +1,4 @@
 import escapeending
 import catwalk
+
+print("Stepping into his manager's office, Stanley was once again stunned to discover not an indication of any human life. Shocked, unraveled, Stanley wondered in disbelief who orchestrated this. What dark secret was being held from him? What he could not have known was that the keypad behind the boss's desk guarded the terrible truth that his boss had been keeping from him. And so the boss had assigned it an extra secret PIN # 2845. But of course, Stanley couldn't possibly have known this.")
