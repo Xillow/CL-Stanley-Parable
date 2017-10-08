c8f0e3a49324e411a9c30d3794484a6b9ce39045
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-08 05:25:44 -0400

    Update areyou.py

--- areyou.py
+++ areyou.py
@@ -1,6 +1,6 @@
 import yes
 import no
-# import time
+import time
 YES = "yes"
 NO = "no"
 
@@ -8,9 +8,9 @@ def areyou():
     sickGag = ""
     
     print("At first, Stanley assumed he had broken the map, until he heard this narration and realized it was part of the game all along. He then praised the game for its insightful and witty commentary into the nature of video game structure and its examination of structural narrative tropes.")
-    # time.sleep(8)
+    time.sleep(8)
     print("So now that you're here, what do you think? Isn't this a fun and unique place to be? Why don't we take a minute just to drink it all in!")
-    # time.sleep(6)
+    time.sleep(6)
     
     while sickGag != YES and sickGag != NO:
         print("Okay I'm over it now. What do you think, are you sick of this gag yet?")
