543afef85b3214a5c058d9164fefc59adf776598
twinkert <thomas.winkert@gmail.com>
2017-10-08 05:32:48 -0400

    Update museum.py

--- museum.py
+++ museum.py
@@ -17,24 +17,25 @@ print("At this point, Stanley was making a conscious, concerted effort to walk f
 
 
 # Metal Jar Crush
-print("As the machine whirled into motion, and Stanley was inched closer and closer to his demise, He reflected that his life had been of no consequence whatsoever.")
+print("As the machine whirled into motion, and Stanley was inched closer and closer to his demise, He reflected that his life had been of no consequence whatsoever.\n")
 
-print("Stanley can't see the bigger picture. He doesn't know the real story. Trapped forever in his narrow vision of what this world is.")
+print("Stanley can't see the bigger picture. He doesn't know the real story. Trapped forever in his narrow vision of what this world is.\n")
 
 print("Perhaps his death was of no great loss. Like plucking the eyeballs from a blind man.")
 
-print("And so he resigned, and willingly accepted this violent end to his brief and shallow life. Farewell Stanley.")
+print("And so he resigned, and willingly accepted this violent end to his brief and shallow life. Farewell Stanley.\n")
 
 
 #female narrator dialouge
 
-print("'Farewell Stanley' cried the Narrator, as Stanley was led helplessly into the enormous metal jaws. In a single visceral instant, Stanley was obliterated, as the machine crushed every bone in his body, killing him instantly.")
+print("'Farewell Stanley' cried the Narrator, as Stanley was led helplessly into the enormous metal jaws. In a single visceral instant, Stanley was obliterated, as the machine crushed every bone in his body, killing him instantly.\n")
 
-print("And yet it would be just a few minutes before Stanley would restart the game, back in his office, as alive as ever. What exactly did the Narrator think he was going to accomplish?")
+print("And yet it would be just a few minutes before Stanley would restart the game, back in his office, as alive as ever. What exactly did the Narrator think he was going to accomplish?\n")
 
-print("When every path you can walk has been created for you long in advance, death becomes meaningless, making life the same. Do you see now? Do you see that Stanley was already dead from the moment he hit Start?")
+print("When every path you can walk has been created for you long in advance, death becomes meaningless, making life the same. Do you see now? Do you see that Stanley was already dead from the moment he hit Start?\n")
 
 #prompt to enter museum
+
 
 
 
