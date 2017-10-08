b67a2972026bde57ce0bfdfdf57195fff1272baa
Xillow <Matthewbee1@gmail.com>
2017-10-08 02:50:49 -0400

    Update door.py

--- door.py
+++ door.py
@@ -6,23 +6,25 @@ MISTAKE_LIM = 4
 
 def door():
 
-    decision = ""
-    count = 0
-
+	decision = ""
+	count = 0
+	
     #while loop checks to make sure you have selected a correct option
-    while (decision != CHOICE_1) and (decision != CHOICE_2):
-        print("All his co-workers were gone. What could it mean? Stanley decided to go to the meeting room; perhaps he had simply missed a memo.")
-        decision = input("(what is your choice?('enter' or 'close')")
+	while (decision != CHOICE_1) and (decision != CHOICE_2):
 
-        if decision == CHOICE_1:
-            window.window()
+		print("All his co-workers were gone. What could it mean? Stanley decided to go to the meeting room; perhaps he had simply missed a memo.")
+		decision = input("what is your choice?('enter' or 'close')")
 
-        elif decision == CHOICE_2:
-            reluctant.reluctant()
-           
-        elif count > MISTAKE_LIM:
-            print("Stanley just kept touching doors and desks and pushing buttons meaninglessly, failing to realize that he was not progressing the story at all.")
+		if decision == CHOICE_1:
+			print("")
+			window.window()	
+		elif decision == CHOICE_2:
+			print("")
+			reluctant.reluctant()
 
-        count += 1
+		elif count > MISTAKE_LIM:
+			
+			print("")
+			print("Stanley just kept touching doors and desks and pushing buttons meaninglessly, failing to realize that he was not progressing the story at all.")
 
-
+		count += 1
