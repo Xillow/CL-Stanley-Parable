47383a6d7bc1709d074126442f295650832d31fa
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-07 20:44:56 -0400

    Update doorchoice.py

--- doorchoice.py
+++ doorchoice.py
@@ -3,14 +3,15 @@ import right
 LEFT = "left"
 RIGHT = "right"
 
-door = "none"
+def doorchoice():
+    door = "none"
 
-while door != LEFT and door != RIGHT:
-    print("When Stanley came to a set of 2 open doors, he entered the door on his left.")
-    door = input("(What is your choice? (\"yes\" or \"no\"))")
+    while door != LEFT and door != RIGHT:
+        print("When Stanley came to a set of 2 open doors, he entered the door on his left.")
+        door = input("(What is your choice? (\"yes\" or \"no\"))")
     
-    if door == LEFT:
-        left.left()
-    if door == RIGHT:
-        right.right()
+        if door == LEFT:
+            left.left()
+        if door == RIGHT:
+            right.right()
         
