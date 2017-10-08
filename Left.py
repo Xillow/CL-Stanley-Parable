b09f8fcabdfda32572405a3106e4fc26bbe9e336
twinkert <thomas.winkert@gmail.com>
2017-10-08 00:53:31 -0400

    Update Left.py

--- Left.py
+++ Left.py
@@ -26,43 +26,45 @@ def Left():
         	print("Entering the broom closet:\n")
         	print("Stanley stepped into the broom closet, but there was nothing here, so he turned around and got back on track.\n")
         	print("There was nothing here. No choice to make. No path to follow. Just an empty broom closet. No reason to still be here.\n")
-		      inCloset = 0
+		      inCloset = 1
+		      CHOICE1 = "leave"
+		      CHOICE2 = "stay"
 		      
 		      while (count < inCloset):
 		      
 		
-		      input("what is your choice?(\"pass\" or \"enter\")
-			if input != CHOICE_1:
+		      input("what is your choice?(\"leave\" or \"stay\")
+			if input != CHOICE1:
 			    print("It was baffling that Stanley was still just sitting in the broom closet. He wasn't even doing anything. At least if there was something to interact with, he'd be justified in some way. As it is, he's literally just standing there, doing sweet FA.")
 			    else:
 			    count += 1
-			input("what is your choice?(\"pass\" or \"enter\")
-			if input != CHOICE_1:
+			input("what is your choice?(\"leave\" or \"stay\")
+			if input != CHOICE1:
 			      print("Are you... Are you really still in the broom closet? Standing around doing nothing? Why? Please offer me some explanation here; I'm- I'm genuinely confused.")
 			      else:
 			      count += 1
-			input("what is your choice?(\"pass\" or \"enter\")
-			if input != CHOICE_1:
+			input("what is your choice?(\"leave\" or \"stay\")
+			if input != CHOICE1:
 			      print("You do realize there's no choice or anything in here right? If I said "Stanley walked past the broom closet" at least you would've had a reason for exploring it to find out. But it didn't even occur to me, because literally, this closet, is of absolutely, no significance to the story, whatsoever. I never would've thought to mention it.\n")
 			      else:
 			      count += 1
-			input("what is your choice?(\"pass\" or \"enter\")
-			if input != CHOICE_1:
+			input("what is your choice?(\"leave\" or \"stay\")
+			if input != CHOICE1:
 			      print("Maybe to you, this is somehow it's own branching path. Maybe, when you go talk about this with your friends, you'll say: "OH! DID U GET THE BROOM CLOSET ENDING? THEB ROOM CLOSET ENDING WAS MY FAVRITE!1 XD" I hope your friends find this concerning.\n")
 			      else:
 			      count += 1
-			input("what is your choice?(\"pass\" or \"enter\")
-			if input != CHOICE_1:
+			input("what is your choice?(\"leave\" or \"stay\")
+			if input != CHOICE1:
 			      print("Stanley was fat and ugly, and really, really stupid. He probably only got the job because of a family connection; that's how stupid he is. That, or with drug money. Also, Stanley is addicted to drugs and hookers.\n")
 			      else:
 			      count += 1
-			input("what is your choice?(\"pass\" or \"enter\")
-			      if input != CHOICE_1:
+			input("what is your choice?(\"leave\" or \"stay\")
+			      if input != CHOICE1:
 			      print("Well, I've come to a very definite conclusion about what's going on right now. You're dead. You got to this broom closet, explored it a bit, and were just about to leave because there's nothing here, when a physical malady of some sort shut down your central nervous system and you collapsed on the keyboard. Well, in a situation like this, the responsible thing is to alert someone nearby so as to ensure that your body is taken care of, before it begins to decompose.\n")
 			      else:
 			      count += 1
-			input("what is your choice?(\"pass\" or \"enter\")
-			      if input != CHOICE_1:
+			input("what is your choice?(\"leave\" or \"stay\")
+			      if input != CHOICE1:
 			      print("HELLO!? ANYONE WHO HAPPENS TO BE NEARBY!! THE PERSON AT THIS COMPUTER IS DEAD!! HE OR SHE HAS FALLEN PREY TO ANY NUMBER OF YOUR COUNTLESS HUMAN PHYSIOLOGICAL VULNERABILITIES. IT'S INDICATIVE OF THE LONG-TERM SUSTAINABILITY OF YOUR SPECIES. PLEASE REMOVE THEIR CORPSE FROM THE AREA AND INSTRUCT ANOTHER HUMAN TO TAKE THEIR PLACE AT THE COMPUTER, MAKING SURE THEY UNDERSTAND BASIC FIRST-PERSON VIDEO GAME MECHANICS, AND FILLING THEM IN ON THE HISTORY OF NARRATIVE TROPES IN VIDEO GAMING, SO THAT THE IRONY AND INSIGHTFUL COMMENTARY OF THIS GAME IS NOT LOST ON THEM.\n")
 			      else:
 			      count += 1
