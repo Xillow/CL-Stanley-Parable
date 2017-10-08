763915387cdae0e5738f125a5bc2a859dbea3955
twinkert <thomas.winkert@gmail.com>
2017-10-08 02:57:49 -0400

    Update boss.py

--- boss.py
+++ boss.py
@@ -1,12 +1,20 @@
 import escapeending
 import catwalk
 
-print("Stepping into his manager's office, Stanley was once again stunned to discover not an indication of any human life. Shocked, unraveled, Stanley wondered in disbelief who orchestrated this. What dark secret was being held from him? What he could not have known was that the keypad behind the boss's desk guarded the terrible truth that his boss had been keeping from him. And so the boss had assigned it an extra secret PIN # 2845. But of course, Stanley couldn't possibly have known this.")
+enter = "enter"
+userInput = ""
 
-print("Stanley began random codes into the keypad, knowing full well that the sheer statistical unlikelihood that this would result in a correct combination. If he knew that the combo was 2-8-4-5, it would be another story entirely. But no. No, this is what he's going to do instead.Stanley began random codes into the keypad, knowing full well that the sheer statistical unlikelihood that this would result in a correct combination. If he knew that the combo was 2-8-4-5, it would be another story entirely. But no. No, this is what he's going to do instead.")
+while (userInput != enter)
+userInput = input("type enter")
 
-print("Stanley just sat around twiddling his thumbs. Trying to input anything into the device was useless, since he could never possibly know that the combination was 2-8-4-5.")
+if userInput == enter
 
-print("2-8-4-5.")
+print("Stepping into his manager's office, Stanley was once again stunned to discover not an indication of any human life. Shocked, unraveled, Stanley wondered in disbelief who orchestrated this. What dark secret was being held from him? What he could not have known was that the keypad behind the boss's desk guarded the terrible truth that his boss had been keeping from him. And so the boss had assigned it an extra secret PIN # 2845. But of course, Stanley couldn't possibly have known this.\n")
 
-print("For god's- but it turns out that the panel's emergency override kicked in, and the door just opened all by itself, and Stanley just got the hell along with the story. Well woop-de-doo.")
+print("Stanley began random codes into the keypad, knowing full well that the sheer statistical unlikelihood that this would result in a correct combination. If he knew that the combo was 2-8-4-5, it would be another story entirely. But no. No, this is what he's going to do instead.Stanley began random codes into the keypad, knowing full well that the sheer statistical unlikelihood that this would result in a correct combination. If he knew that the combo was 2-8-4-5, it would be another story entirely. But no. No, this is what he's going to do instead.\n")
+
+print("Stanley just sat around twiddling his thumbs. Trying to input anything into the device was useless, since he could never possibly know that the combination was 2-8-4-5.\n")
+
+print("2-8-4-5.\n")
+
+print("For god's- but it turns out that the panel's emergency override kicked in, and the door just opened all by itself, and Stanley just got the hell along with the story. Well woop-de-doo.\n")
