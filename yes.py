09a9382e76954dd52902ae7adc8421710f425657
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-07 23:50:43 -0400

    Update yes.py

--- yes.py
+++ yes.py
@@ -3,5 +3,66 @@ ESCAPE = "escape"
 
 def yes():
     print("(Press any key to continue.)")
-    continue = input("Well, I don't know how to say this politely but you could literally just hit escape and restart the game any old time you want.")
-    if 
+    userInput = input("Well, I don't know how to say this politely but you could literally just hit escape and restart the game any old time you want. ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("Like right now! You could have done it just then! ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("Now would also be an appropriate time to quit! Any one of these points, and so many many more, all of them are appropriate! I'm enjoying what seems to be an internal conflict going on where you are literally unable to act on your own desires to restart the game, ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("so just to push the envelope, i'm going to try to make this as miserable as possible and we'll see how long you can maintain. ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*There once was a man named Stanley,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*Who people considered so manly.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*But the truth must be told,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*He was not very old,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*And was quite particularly gangly.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*What Stanley liked most was buttons.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*He pushed them like some kind of glutton.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*He did it all day* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*In a meaningful way,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*But his brain had long ceased to function.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*Which is why he is in this parable,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*And lives an existence quite terrible.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*And if you are not strong,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*And keep playing along,* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*You too will become quite unbearable.* ")
+    if userInput == ESCAPE:
+        door.door()
+    userInput = input("*You too will become quite unbearable, yeah!* ")
+    if userInput == ESCAPE:
+        door.door()
+    while userInput != ESCAPE:
+        userInput = input("...")
+    door.door()
