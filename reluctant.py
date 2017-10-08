d04c2c0e89a86fc469371da8f94a88a8d81d349d
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-07 21:33:27 -0400

    Update reluctant.py

--- reluctant.py
+++ reluctant.py
@@ -1,20 +1,22 @@
 import door
-import time
+# import time
 
 def reluctant():
     print("But Stanley simply couldn't handle the pressure. What if he had to make a decision? What if a crucial outcome fell under his responsibility? He had never been trained for that! No, this couldn't go any way except badly.")
-    time.sleep(8)
+    # time.sleep(8)
     print("The right thing to do now, Stanley thought to himself, is to wait. Nothing will hurt me. Nothing will break me. In here, I can be happy, forever. I will be happy.")
-    time.sleep(8)
+    # time.sleep(8)
     print("Stanley waited. Hours passed. Then days. Had years gone by? He no longer had the ability to tell. But the one thing he knew for sure, beyond any doubt, was that if he waited long enough, the answers would come.")
-    time.sleep(8)
+    # time.sleep(8)
     print("Eventually, some day, they would arrive. Soon, very soon now, this will end. He will be spoken to. He will be told what to do.")
-    time.sleep(5)
+    # time.sleep(5)
     print("Now it's just a little bit closer.")
-    time.sleep(1)
+    # time.sleep(1)
     print("Now it's even closer.")
-    time.sleep(1)
+    # time.sleep(1)
     print("Here it comes.")
-    time.sleep(1)
+    # time.sleep(1)
+    
+    # clear
     
     door.door()
