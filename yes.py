59eb0551f0228f1150596870d388e54afc56aa16
macath1 <32472689+macath1@users.noreply.github.com>
2017-10-07 22:02:04 -0400

    Create yes.py

---
+++ yes.py
@@ -0,0 +1,7 @@
+import door
+ESCAPE = "escape"
+
+def yes():
+    print("(Press any key to continue.)")
+    continue = input("Well, I don't know how to say this politely but you could literally just hit escape and restart the game any old time you want.")
+    if 
