c2bc56ed7d6be6ba9fcc740a5efd6c3529437396
Xillow <Matthewbee1@gmail.com>
2017-10-08 02:48:35 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,18 +2,19 @@ import door
 
 
 def main():
-    print("This is the story of a man named Stanley.")
-    print("")
-    print("Stanley worked for a company in a big building where he was Employee #427. Employee #427's job was simple: he") 
-    print("sat at his desk in room 427 and he pushed buttons on a keyboard. Orders came to him through a monitor on his")
-    print("desk, telling him what buttons to push, how long to push them, and in what order. This is what Employee #427 did")
-    print("every day of every month of every year, and although others might have considered it soul rending, Stanley relished")
-    print("every moment the orders came in, as though he had been made exactly for this job. And Stanley was happy.")  
+	print("This is the story of a man named Stanley.")
+	print("")
+	print("Stanley worked for a company in a big building where he was Employee #427. Employee #427's job was simple: he") 
+	print("sat at his desk in room 427 and he pushed buttons on a keyboard. Orders came to him through a monitor on his")
+	print("desk, telling him what buttons to push, how long to push them, and in what order. This is what Employee #427 did")
+	print("every day of every month of every year, and although others might have considered it soul rending, Stanley relished")
+	print("every moment the orders came in, as though he had been made exactly for this job. And Stanley was happy.")
+	print("")
+	
+	door.door()
   
-    door.door()
   
   
-  
-  main()
+main()
     
    
