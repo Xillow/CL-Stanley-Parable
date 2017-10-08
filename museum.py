3cddeb93091a80331a9cca673cf130769e9af03b
twinkert <thomas.winkert@gmail.com>
2017-10-08 05:29:08 -0400

    Create museum.py

---
+++ museum.py
@@ -0,0 +1,46 @@
+import door
+
+
+
+
+def museum()
+
+
+
+print("Although this passageway had the word 'Escape' written on it, the truth was, that at the end of this hall, Stanley would meet his violent death.\n")
+
+print("The door behind him was not shut. Stanley still had every opportunity to turn around and get back on track.\n")
+
+print("At this point, Stanley was making a conscious, concerted effort to walk forward, and willingly confront his death."\n)
+
+
+
+
+# Metal Jar Crush
+print("As the machine whirled into motion, and Stanley was inched closer and closer to his demise, He reflected that his life had been of no consequence whatsoever.")
+
+print("Stanley can't see the bigger picture. He doesn't know the real story. Trapped forever in his narrow vision of what this world is.")
+
+print("Perhaps his death was of no great loss. Like plucking the eyeballs from a blind man.")
+
+print("And so he resigned, and willingly accepted this violent end to his brief and shallow life. Farewell Stanley.")
+
+
+#female narrator dialouge
+
+print("'Farewell Stanley' cried the Narrator, as Stanley was led helplessly into the enormous metal jaws. In a single visceral instant, Stanley was obliterated, as the machine crushed every bone in his body, killing him instantly.")
+
+print("And yet it would be just a few minutes before Stanley would restart the game, back in his office, as alive as ever. What exactly did the Narrator think he was going to accomplish?")
+
+print("When every path you can walk has been created for you long in advance, death becomes meaningless, making life the same. Do you see now? Do you see that Stanley was already dead from the moment he hit Start?")
+
+#prompt to enter museum
+
+
+
+
+# PUT WAIT TRACK HERE
+
+
+
+
