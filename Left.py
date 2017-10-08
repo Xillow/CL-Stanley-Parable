82638bdafbf43b3baf0af1567ab56da11d993230
twinkert <thomas.winkert@gmail.com>
2017-10-08 00:39:18 -0400

    Update Left.py

--- Left.py
+++ Left.py
@@ -34,8 +34,43 @@ def Left():
 		      input("what is your choice?(\"pass\" or \"enter\")
 			if input != CHOICE_1:
 			    print("It was baffling that Stanley was still just sitting in the broom closet. He wasn't even doing anything. At least if there was something to interact with, he'd be justified in some way. As it is, he's literally just standing there, doing sweet FA.")
-			    elif:
+			    else:
 			    count += 1
+			input("what is your choice?(\"pass\" or \"enter\")
+			if input != CHOICE_1:
+			      print("Are you... Are you really still in the broom closet? Standing around doing nothing? Why? Please offer me some explanation here; I'm- I'm genuinely confused.")
+			      else:
+			      count += 1
+			input("what is your choice?(\"pass\" or \"enter\")
+			if input != CHOICE_1:
+			      print("You do realize there's no choice or anything in here right? If I said "Stanley walked past the broom closet" at least you would've had a reason for exploring it to find out. But it didn't even occur to me, because literally, this closet, is of absolutely, no significance to the story, whatsoever. I never would've thought to mention it.\n")
+			      else:
+			      count += 1
+			input("what is your choice?(\"pass\" or \"enter\")
+			if input != CHOICE_1:
+			      print("Maybe to you, this is somehow it's own branching path. Maybe, when you go talk about this with your friends, you'll say: "OH! DID U GET THE BROOM CLOSET ENDING? THEB ROOM CLOSET ENDING WAS MY FAVRITE!1 XD" I hope your friends find this concerning.\n")
+			      else:
+			      count += 1
+			input("what is your choice?(\"pass\" or \"enter\")
+			if input != CHOICE_1:
+			      print("Stanley was fat and ugly, and really, really stupid. He probably only got the job because of a family connection; that's how stupid he is. That, or with drug money. Also, Stanley is addicted to drugs and hookers.\n")
+			      else:
+			      count += 1
+			input("what is your choice?(\"pass\" or \"enter\")
+			      if input != CHOICE_1:
+			      print("Well, I've come to a very definite conclusion about what's going on right now. You're dead. You got to this broom closet, explored it a bit, and were just about to leave because there's nothing here, when a physical malady of some sort shut down your central nervous system and you collapsed on the keyboard. Well, in a situation like this, the responsible thing is to alert someone nearby so as to ensure that your body is taken care of, before it begins to decompose.\n")
+			      else:
+			      count += 1
+			input("what is your choice?(\"pass\" or \"enter\")
+			      if input != CHOICE_1:
+			      print("HELLO!? ANYONE WHO HAPPENS TO BE NEARBY!! THE PERSON AT THIS COMPUTER IS DEAD!! HE OR SHE HAS FALLEN PREY TO ANY NUMBER OF YOUR COUNTLESS HUMAN PHYSIOLOGICAL VULNERABILITIES. IT'S INDICATIVE OF THE LONG-TERM SUSTAINABILITY OF YOUR SPECIES. PLEASE REMOVE THEIR CORPSE FROM THE AREA AND INSTRUCT ANOTHER HUMAN TO TAKE THEIR PLACE AT THE COMPUTER, MAKING SURE THEY UNDERSTAND BASIC FIRST-PERSON VIDEO GAME MECHANICS, AND FILLING THEM IN ON THE HISTORY OF NARRATIVE TROPES IN VIDEO GAMING, SO THAT THE IRONY AND INSIGHTFUL COMMENTARY OF THIS GAME IS NOT LOST ON THEM.\n")
+			      else:
+			      count += 1
+
+			      
+			      
+			   
+			    
 			    
 			    
 			 
@@ -53,58 +88,4 @@ def Left():
 
                 
                 
-                
-                decision = input("what is your choice?(\"enter\" or \"close\")
-                        
-                
-                        if input == CHOICE_1:
-        print("It was baffling that Stanley was still just sitting in the broom closet. He wasn't even doing anything. At least if there was something to interact with, he'd be justified in some way. As it is, he's literally just standing there, doing sweet FA.")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		            elif input == CHOICE_1:
-        print("Are you... Are you really still in the broom closet? Standing around doing nothing? Why? Please offer me some explanation here; I'm- I'm genuinely confused.")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                elif input == CHOICE_1:
-        print("You do realize there's no choice or anything in here right? If I said "Stanley walked past the broom closet" at least you would've had a reason for exploring it to find out. But it didn't even occur to me, because literally, this closet, is of absolutely, no significance to the story, whatsoever. I never would've thought to mention it.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                    elif input == CHOICE_1:
-        print("Maybe to you, this is somehow it's own branching path. Maybe, when you go talk about this with your friends, you'll say: "OH! DID U GET THE BROOM CLOSET ENDING? THEB ROOM CLOSET ENDING WAS MY FAVRITE!1 XD" I hope your friends find this concerning.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                        elif input == CHOICE_1:
-        print("Stanley was fat and ugly, and really, really stupid. He probably only got the job because of a family connection; that's how stupid he is. That, or with drug money. Also, Stanley is addicted to drugs and hookers.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                            elif input == CHOICE_1:
-        print("Well, I've come to a very definite conclusion about what's going on right now. You're dead. You got to this broom closet, explored it a bit, and were just about to leave because there's nothing here, when a physical malady of some sort shut down your central nervous system and you collapsed on the keyboard. Well, in a situation like this, the responsible thing is to alert someone nearby so as to ensure that your body is taken care of, before it begins to decompose.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                elif input == CHOICE_1:
-        print("HELLO!? ANYONE WHO HAPPENS TO BE NEARBY!! THE PERSON AT THIS COMPUTER IS DEAD!! HE OR SHE HAS FALLEN PREY TO ANY NUMBER OF YOUR COUNTLESS HUMAN PHYSIOLOGICAL VULNERABILITIES. IT'S INDICATIVE OF THE LONG-TERM SUSTAINABILITY OF YOUR SPECIES. PLEASE REMOVE THEIR CORPSE FROM THE AREA AND INSTRUCT ANOTHER HUMAN TO TAKE THEIR PLACE AT THE COMPUTER, MAKING SURE THEY UNDERSTAND BASIC FIRST-PERSON VIDEO GAME MECHANICS, AND FILLING THEM IN ON THE HISTORY OF NARRATIVE TROPES IN VIDEO GAMING, SO THAT THE IRONY AND INSIGHTFUL COMMENTARY OF THIS GAME IS NOT LOST ON THEM.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                   elif input == CHOICE_1:
-        print("Alright, when you've done that, just step out into the hallway.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                      elif input == CHOICE_1:
-        print("[Player steps out into the hallway]\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                         elif input == CHOICE_1:
-        print("Ah, second player! It's good to have you on board. I guarantee you can't do any worse than the person who came before you.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                             elif input == CHOICE_1:
-        print("[Player steps back into the broom closet]\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                                 elif input == CHOICE_1:
-        print("You too? Unbelievable. I'm at the mercy of an entire species, of invalids. Perhaps there's a monkey nearby you can hand the controls too. A fish, fungus. Look, you can hammer out the details, I'm not particularly picky. I'll just be waiting for when you're ready to pick up the story again.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                                     elif input == CHOICE_1:
-        print("[Later on in the game, when the Player goes back into the broom closet]\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                                         elif input == CHOICE_1:
-        print("Oh, no! Oh, no no no no no no no no no no no no, not again- I won't be part of this- I'm not going to encourage you- I'm not going to say anything at all. I'm just going to be patient and wait for you to finish whatever it is you enjoy doing so much in this room. Please, take your time.\n")
-		decision = input("what is your choice?(\"enter\" or \"close\")
-		                                                             else input == CHOICE_1:
-        print("Getting to the staircase:\n")
-        print("Coming to a staircase, Stanley walked upstairs to his boss's office.\n")
-		decision = input("You have to continue?(\"close\")
-		
-
- 
-
-  
+            
